c2adcad09879c35ca84e1109ba1dfe5f4f3b08ad
Roque Alberto Pulido Morales <rapulidom@desarrollo-ultrasist.com.mx>
2025-04-06 12:35:07 -0600

    W05: Chamber Discover Page

--- chamber/scripts/discover.js
+++ chamber/scripts/discover.js
@@ -24,3 +24,43 @@ hamButton.addEventListener("click", () => {
   navigation.classList.toggle("open");
   hamButton.classList.toggle("open");
 });
+
+// Card Infomation
+
+const cardsBuild = (info) => {
+  return /*html*/ `
+    <div class="card">
+        <h2 class="title">${info.name}</h2>
+            <figure>
+                <img src="${info.image}" alt="${info.name}" loading="lazy" />
+                <figcaption>${info.name}</figcaption>
+            </figure>
+        <div class="card-content">
+            <div class="card-info">
+                <p><span class="label">Address:</span>${info.address}</p>
+                <p><span class="label">Description:</span>${info.description}</p>
+                </div>
+            <button>learn more</button>
+        </div>
+    </div>`;
+};
+
+const requestData = async () => {
+  const response = await fetch("data/discover.json");
+  const data = await response.json();
+  return data;
+};
+
+renderCards = async (classContainer) => {
+  const data = await requestData();
+  if (!data || data.length === 0) {
+    console.error("No data available to render cards.");
+    return;
+  }
+  const container = document.querySelector(classContainer);
+
+  container.innerHTML = ""; // Clear the container before rendering new cards
+  container.innerHTML = data.map(cardsBuild).join("");
+};
+
+renderCards(".container-discover");
